9cc34385bcfb55637308baf8c96fbf4072c15460
miscoined <miscoined@gmail.com>
2017-08-19 16:40:50 +1000

    Add Steam OpenID login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_openid import OpenID
+
 app = Flask(__name__)
+app.config.from_object('config')
+
+oid = OpenID(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from flask import render_template
-from app import app
+from app import app, oid
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+@oid.loginhandler
+def login():
+    return oid.try_login('http://steamcommunity.com/openid')
+
+
+@oid.after_login
+def after_login(resp):
+    return str(resp)
