2a3c17eb6009e595763e15ff9ad9fb2ac209337d
Naris <nariscatboy@gmail.com>
2017-08-19 18:25:11 +1000

    reorganized rec.py

--- rec.py
+++ rec.py
@@ -2,30 +2,43 @@ from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark import SparkContext
 from pprint import pprint as pp
 
+sc = SparkContext()
+try:
+    model = MatrixFactorizationModel.load(sc, "CF.model")
+except Excetion:
+    model = None
+
 def main():
-
-    sc = SparkContext()
-    # Load and parse the data
-    data = sc.textFile("test.data")
-    ratings = data.map(lambda l: l.split(','))\
-    .map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
-
-    # Build the recommendation model using Alternating Least Squares
-    rank = 10
-    numIterations = 10
-    model = ALS.train(ratings, rank, numIterations)
-
+    data = load_file("test.data")
+    model = train(data)
     # Evaluate the model on training data
+    '''
     testdata = ratings.map(lambda p: (p[0], p[1]))
     predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
     ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
     MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
     print("Mean Squared Error = " + str(MSE))
+    '''
+    pp(get_rec(1))
 
-    # Save and load modeil
+def train(data):
+    '''
+    insert documentation
+    '''
+    global model
+    # Build the recommendation model using Alternating Least Squares
+    rank = 10
+    numIterations = 10
+    model = ALS.train(data, rank, numIterations)
     # model.save(sc, "CF.model")
-    # model = MatrixFactorizationModel.load(sc, "CF.model")
-    user_recs = model.recommendProducts(1, 10)
-    pp(user_recs)
+    return model
+
+def get_rec(user, num_rec=10):
+    global model
+    return model.recommendProducts(user, num_rec)
+
+def load_file(filename):
+    global sc
+    return sc.textFile(filename).map(lambda l: l.split(',')).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
 
 main()
