55bfa9b54e4f178b0213ec7d0f0d559b1b9fa831
miscoined <miscoined@gmail.com>
2017-08-19 20:42:03 +1000

    Clean up Steam API calls
    Add a `_steam_endpoint` function to cleanly generate an API endpoint url.

--- app/user_info.py
+++ app/user_info.py
@@ -1,23 +1,42 @@
 import requests
 import csv
+from urllib.parse import urljoin
 from app import app
+
+def _steam_endpoint(endpoint, apikey=app.config['STEAM_API_KEY'], **params):
+    '''
+    Return a formatted endpoint URL from the Steam API
+    '''
+    url = [urljoin("http://api.steampowered.com/", endpoint)]
+
+    if apikey:
+        params["key"] = apikey
+    paramstring = "&".join([f"{k}={params[k]}" for k in params])
+    if paramstring:
+        url.append("?" + paramstring)
+
+    return urljoin(*url)
 
 def get_user_data(steam_id):
     '''
-    Creates a csv file called training_data with the format: (steamid,gameid,play_time)
-
+    Create a csv file called training_data with the format: (steamid,gameid,play_time)
     '''
-    g_api = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + app.config['STEAM_API_KEY'] + '&steamid=' + str(steam_id) + '&format=json'
-    output = []
-    game_data = requests.get(g_api).json()
+    url = _steam_endpoint('IPlayerService/GetOwnedGames/v0001', steamid=steam_id, format="json")
+    game_data = requests.get(url).json()
+    print(len(filter_unplayed(game_data)))
+
     with open('training_data','w') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i in game_data['response']['games']:
-            writer.writerow([steam_id,i['appid'],i['playtime_forever']])
+            writer.writerow([steam_id, i['appid'], i['playtime_forever']])
 
 def filter_unplayed(game_data):
+    '''
+    Filter out unplayed games from the data set
+    '''
     unplayed_games = []
     for i in game_data['response']['games']:
         if i['playtime_forever'] == 0:
             unplayed_games.append(i['appid'])
     return unplayed_games
+
