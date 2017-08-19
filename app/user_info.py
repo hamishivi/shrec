eb4aeb02fc758f301356dfd5c557f52b86899346
Anuj Dhavalikar <anuj.dhavalikar@gmail.com>
2017-08-19 21:07:51 +1000

    Add friend traversal method

--- app/user_info.py
+++ app/user_info.py
@@ -1,17 +1,18 @@
 import requests
 import csv
+import pprint
 from app import app
 
 def get_user_data(steam_id):
     '''
-    Creates a csv file called training_data with the format: (steamid,gameid,play_time)
+    Create a csv file called training_data with the format: (steamid,gameid,play_time)
 
     '''
     g_api = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + app.config['STEAM_API_KEY'] + '&steamid=' + str(steam_id) + '&format=json'
     output = []
     game_data = requests.get(g_api).json()
     print(len(filter_unplayed(game_data)))
-    with open('training_data','w') as f:
+    with open('training_data','a') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i in game_data['response']['games']:
             writer.writerow([steam_id,i['appid'],i['playtime_forever']])
@@ -22,3 +23,23 @@ def filter_unplayed(game_data):
         if i['playtime_forever'] == 0:
             unplayed_games.append(i['appid'])
     return unplayed_games
+
+def traverse_friend_graph(steam_id,visited=set(),depth=0):
+	if depth > 2:
+		print('Recursion depth reached')
+		return visited
+	visited.add(steam_id)
+	pp = pprint.PrettyPrinter(indent=4)
+	f_api = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=' + app.config['STEAM_API_KEY'] + '&steamid=' + str(steam_id) + '&relationship=friend'
+	friends = requests.get(f_api).json()
+	for i in friends['friendslist']['friends']:
+		if i['steamid'] in visited:
+			continue
+		try:
+			visited |= traverse_friend_graph(i['steamid'],visited,depth=depth+1)
+		except KeyError:
+			print("no friend scrub",steam_id)
+			print(i)
+	print(visited)
+	return visited
+
