c7cab477f2965ad7b647c1bd57997756832d63db
Hamish Ivison <hamishivi@gmail.com>
2017-12-03 11:17:30 +1100

    Add handling for unlisted games

--- app/game_info.py
+++ app/game_info.py
@@ -11,10 +11,16 @@ def get_game_info(app_id):
     g_api = 'http://store.steampowered.com/api/appdetails/?appids=' + str(app_id) + '&format=json'
     output = []
     game_data = requests.get(g_api).json()
-    #print(game_data)
     if not game_data or 'data' not in game_data[str(app_id)]:
-        return ("Broken Game", app_id, "This game doesn't exist!?", "No image :'(")
-    game_name = game_data[str(app_id)]['data']['name']
+        # broken game, look it up on steam-tracker
+        game_name = get_unlisted_name(app_id)
+        game_image = "Sorry, no image found"
+        # no help here :'(
+        if game_name is None:
+            return ("Broken Game", app_id, "This game doesn't exist!?", "No image :'(")
+    else:
+        game_name = game_data[str(app_id)]['data']['name']
+        game_image = game_data[str(app_id)]['data']['header_image']
     game_description = get_game_description(game_name)
     # fall back on steam api
     if (len(game_description) == 0):
@@ -27,7 +33,6 @@ def get_game_info(app_id):
             game_description = game_description[:num] + '...'
         except Exception:
             print('Something went wrong with game description extraction')
-    game_image = game_data[str(app_id)]['data']['header_image']
     return (game_name, app_id, game_description, game_image)
 
 
@@ -42,3 +47,11 @@ def get_game_description(game_name):
         return res['summary']
     else:
         return ''
+
+def get_unlisted_name(game_id):
+    st_api = 'https://steam-tracker.com/api?action=GetAppList'
+    res = requests.get(st_api).json()['removed_apps']
+    for app in res:
+        if app['appid'] == str(game_id):
+            return app['name']
+    return None
