b8e3a776af08f7706b3db32e8fc2dd740648aef1
Naris R <nariscatboy@gmail.com>
2017-12-03 23:07:39 +1100

    made cache actually count hits

--- app/cache.py
+++ app/cache.py
@@ -7,13 +7,14 @@ cache = {}
 cache_limit = 50
 
 def get(url, **kwargs):
-    if url in cache:
-        return cache[url][1]
-    else:
+    try:
+        hit, response = cache[url]
+        cache[url] = (hit + 1, response)
+        return response
+    except KeyError:
         response = requests.get(url, **kwargs)
         cache[url] = (0, response)
         # check if hit limit
         if len(cache) > cache_limit:
-            # remove least used item from cache
             cache.pop(min(cache.keys(), key=lambda url: cache[url][0]))
         return response
