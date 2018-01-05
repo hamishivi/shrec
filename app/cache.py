1fd11209d5f6de99a9ebfa72227a7c2457952f9b
Naris <nariscatboy@gmail.com>
2018-01-05 14:07:41 +1100

    rediscovered wheels

--- app/cache.py
+++ app/cache.py
@@ -1,19 +1,4 @@
 import requests
+import functools
 
-# cache has format url: usage, response
-cache = {}
-# number of items we want to keep
-cache_limit = 50
-
-def get(url, **kwargs):
-    try:
-        hit, response = cache[url]
-        cache[url] = (hit + 1, response)
-        return response
-    except KeyError:
-        response = requests.get(url, **kwargs)
-        cache[url] = (0, response)
-        # check if hit limit
-        if len(cache) > cache_limit:
-            cache.pop(min(cache.keys(), key=lambda url: cache[url][0]))
-        return response
+get = functools.lru_cache(maxsize=128)(requests.get)
