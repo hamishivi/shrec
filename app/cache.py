9a9cc964c3c8107353e801e1700bdfd3c1a50dc2
Naris <nariscatboy@gmail.com>
2018-01-05 16:49:20 +1100

    decoupled game_info

--- app/cache.py
+++ app/cache.py
@@ -2,3 +2,10 @@ import requests
 import functools
 
 get = functools.lru_cache(maxsize=128)(requests.get)
+
+class HashableDict(dict):
+    def __setitem__(self, key, value):
+        raise TypeError('Hashable means Immutable')
+
+    def __hash__(self):
+        return hash(frozenset(self.items()))
